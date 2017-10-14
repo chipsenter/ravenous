fd97544d5c412221f1e1ee8b2302542261124d15
Johan <jlindstrom@lifetimefitness.com>
2017-10-14 12:12:27 -0600

    2nd part of the project done

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,15 +5,14 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
     render() {
         return (
-            <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-          </div>
-        );
-    }
+        <div className="BusinessList">
+        {
+            this.props.businesses.map(business => <Business business={business} />
+            ) 
+        }
+        </div>
+        );       
+        
+     }
 }
 export default BusinessList; 
