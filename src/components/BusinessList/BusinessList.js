ccd31a3b20d6f77b9b4d21bd4fef5da2ae63aed2
Johan <jlindstrom@lifetimefitness.com>
2017-10-18 10:12:18 -0600

    Completed 3rd part of Ravenous

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,16 +3,16 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-        <div className="BusinessList">
+  render() {
+    return (
+      <div className="BusinessList">
         {
-            this.props.businesses.map(business => <Business business={business} />
-            ) 
-        }
-        </div>
-        );       
-        
-     }
+          this.props.businesses.map(business =>
+            <Business business={business} />
+        )
+      }
+      </div>
+    );
+  }
 }
-export default BusinessList; 
+export default BusinessList;
